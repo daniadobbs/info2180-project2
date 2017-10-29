4d5fc667508bf28e0b08b2ef3b7fffea560105e3
daniadobbs <dania.dobbs@gmail.com>
2017-10-29 10:26:27 -0500

    added puzzlepiece property to tiles

---
+++ fifteen.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//var num_tiles= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+
+var puzzlearea= $(#puzzlearea);
+var tiles= puzzlearea.children;
+
+for(var i=0; i<tile.length;i++){
+	addClass(puzzlepieces);
+}
